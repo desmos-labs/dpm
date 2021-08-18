5f0781cfde258dd4e5b40feeca89f8e29f32d80b
Manuel Turetta <manuel.turetta94@gmail.com>
2021-08-18 18:45:40 +0100

    Documented Account and Wallet source

--- src/sources/AccountSource.ts
+++ src/sources/AccountSource.ts
@@ -3,7 +3,11 @@ import ChainAccount from '../types/chainAccount';
 
 declare type SourceCache = Map<string, ChainAccount>;
 
+/**
+ * Data source class to access the user's accounts stored into the device storage.
+ */
 class AccountSource {
+
     private readonly ACCOUNTS_KEY = 'accounts';
     private _cache: SourceCache | null;
 
@@ -11,6 +15,10 @@ class AccountSource {
         this._cache = null;
     }
 
+    /**
+     * Utility function to interact with the cached values.
+     * @param reader - function that will have access and can interact with the cache.
+     */
     async useCache<T>(reader: (cache: SourceCache) => Promise<T>): Promise<T> {
         if (this._cache === null) {
             try {
@@ -29,12 +37,20 @@ class AccountSource {
         return reader(this._cache!);
     }
 
+    /**
+     * Gets all the user's accounts.
+     */
     public async getAllAccounts(): Promise<ChainAccount[]> {
         return this.useCache(async cache => {
             return Array.from(cache.values());
         });
     }
 
+    /**
+     * Inserts an account into the device storage, if exist an account with the same bech32 address
+     * the previous account will be overwritten.
+     * @param account - Account that will be inserted.
+     */
     public async putAccount(account: ChainAccount): Promise<void> {
         const cache = await this.useCache(async cache => {
             cache.set(account.address, account);
@@ -44,17 +60,21 @@ class AccountSource {
         await SecureStorage.setItem(this.ACCOUNTS_KEY, json);
     }
 
-    public async getAccount(address: string): Promise<ChainAccount> {
+    /**
+     * Gets an account by it's bech32 address.
+     * @param address - The bech32 address of the account of interest.
+     */
+    public async getAccount(address: string): Promise<ChainAccount | null> {
         return this.useCache(async cache => {
             const account = cache.get(address);
-            if (account === undefined) {
-                throw new Error("Can't find account with address: " + address);
-            }
-
-            return account;
+            return account !== undefined ? account : null;
         });
     }
 
+    /**
+     * Removes an account from the device storage.
+     * @param account - The account that will be removed.
+     */
     public async removeAccount(account: ChainAccount): Promise<void> {
         const cache = await this.useCache(async cache => {
             cache.delete(account.address);
@@ -65,6 +85,6 @@ class AccountSource {
     }
 }
 
+// Singleton instance that will be exposed.
 const _AccountSource = new AccountSource();
-
 export default _AccountSource;
--- src/sources/LocalWalletsSource.ts
+++ src/sources/LocalWalletsSource.ts
@@ -1,7 +1,14 @@
 import * as SecureStorage from '../utilils/SecureStorage';
 import LocalWallet from '../wallet/LocalWallet';
 
+/**
+ * Source that contains the user's wallets.
+ */
 export class LocalWalletsSource {
+
+    /**
+     * Gets the bech32 addresses of al the user's wallets.
+     */
     public async getAllAddresses(): Promise<string[]> {
         try {
             const content = await SecureStorage.getItem('addresses');
@@ -17,47 +24,100 @@ export class LocalWalletsSource {
         return [];
     }
 
+    /**
+     * Puts a wallet into the device storage.
+     * @param wallet - The wallet to be saved.
+     * @param password - Password used to protect the user wallet.
+     * @param accessWithBiometrics - True if the wallet can be unlocked with the biometrics instead of the password.
+     */
     public async putWallet(
         wallet: LocalWallet,
         password: string,
-        biometricProtected?: boolean,
+        accessWithBiometrics?: boolean,
     ): Promise<void> {
         const addresses = await this.getAllAddresses();
-
+        // Saves the wallet address.
         if (addresses.indexOf(wallet.bech32Address) === -1) {
             addresses.push(wallet.bech32Address);
             await SecureStorage.setItem('addresses', JSON.stringify(addresses));
         }
 
-        await SecureStorage.setItem(
-            `${wallet.bech32Address}_wallet`,
-            wallet.serialize(),
+        if (accessWithBiometrics === true) {
+            // If can be accessed with the biometrics secure the wallet password with the user's biometrics.
+            await SecureStorage.setItem(`${wallet.bech32Address}_key`, password,
+                {
+                    biometrics: true,
+                },
+            );
+        } else {
+            await SecureStorage.deleteItem(`${wallet.bech32Address}_key`);
+        }
+
+        await SecureStorage.setItem(`${wallet.bech32Address}_wallet`, wallet.serialize(),
             {
                 password,
-                biometrics: biometricProtected,
             },
         );
     }
 
+    /**
+     * Gets a wallet from the device storage.
+     * @param address - bech32 address of the wallet of interest.
+     * @param password - Password to decipher the wallet.
+     * @param useBiometrics - If true the password field will be ignored and the wallet will be unlocked using.
+     * the user's biometrics.
+     */
     public async getWallet(
         address: string,
-        password: string,
-        biometricProtected?: boolean,
+        password?: string,
+        useBiometrics?: boolean,
     ): Promise<LocalWallet> {
-        const serialized = await SecureStorage.getItem(`${address}_wallet`, {
-            password,
-            biometrics: biometricProtected,
+        let walletPassword: string | null = password ?? null;
+        if (useBiometrics === true) {
+            walletPassword = await SecureStorage.getItem(`${address}_key`,
+                {
+                    biometrics: true,
+                },
+            );
+        }
+
+        if (walletPassword === null) {
+            if (useBiometrics === true) {
+                throw new Error(`Biometrics not enabled for wallet with address ${address}`)
+            } else {
+                throw new Error("Can't access the wallet without the password")
+            }
+        }
+
+        let serializedWallet = await SecureStorage.getItem(`${address}_wallet`, {
+            password: walletPassword,
         });
 
-        if (serialized === null) {
+
+        if (serializedWallet === null) {
             throw new Error(`Can't find wallet for address ${address}`);
         }
 
-        return LocalWallet.deserialize(serialized);
+        return LocalWallet.deserialize(serializedWallet);
     }
 
+    /**
+     * Deletes a wallet from the device storage
+     * @param address - bech32 address of the wallet to delete.
+     */
     public async removeWallet(address: string): Promise<void> {
-        await SecureStorage.deleteItem(address);
+        // Remove the address from the cache
+        const addresses = await this.getAllAddresses();
+        const addressIndex = addresses.indexOf(address);
+        if (addressIndex !== -1) {
+            addresses.splice(addressIndex, 1);
+            await SecureStorage.setItem('addresses', JSON.stringify(addresses));
+        }
+
+        // Remove the wallet
+        await SecureStorage.deleteItem(`${address}_wallet`);
+        // Remove the key
+        await SecureStorage.deleteItem(`${address}_key`);
     }
 }
 
