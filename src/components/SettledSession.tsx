d177415b42f80cc920085df71c1f4ca8d58426dc
Manuel Turetta <manuel.turetta94@gmail.com>
2021-09-20 18:20:55 +0100

    Made the SettledSession locale sensitive

--- src/components/SettledSession.tsx
+++ src/components/SettledSession.tsx
@@ -1,34 +1,48 @@
 import React from "react";
 import {SessionTypes} from "@walletconnect/types";
-import {Button, NativeSyntheticEvent, NativeTouchEvent, Text, View} from "react-native";
 import useWalletConnectDisconnect, {SessionStatus} from "../hooks/useWalletConnectTerminate";
+import {IconButton, List} from "react-native-paper";
+import {IconSource} from "react-native-paper/src/components/Icon";
+import {useTranslation} from "react-i18next";
 
 export type Props = {
     session: SessionTypes.Settled
 }
 export default function SettledSession(props: Props) {
     const [status, disconnect] = useWalletConnectDisconnect()
+    const {t} = useTranslation();
 
-    const onDisconnectPressed = (_: NativeSyntheticEvent<NativeTouchEvent>) => {
+    const onDisconnectPressed = () => {
         disconnect(props.session.topic)
     }
 
     if (status === null) {
-        return <View>
-            <Text>{props.session.peer.metadata.name}</Text>
-            <Text>{props.session.peer.metadata.url}</Text>
-            <Button title={"Disconnect"} onPress={onDisconnectPressed} />
-        </View>
+        const icons = props.session.peer.metadata.icons;
+        let icon: IconSource = "puzzle"
+        if (icons.length > 0) {
+            icon = {
+                uri: props.session.peer.metadata.icons[0]
+            }
+        }
+        return <List.Item
+            title={props.session.peer.metadata.name}
+            description={props.session.peer.metadata.url}
+            left={() => <List.Icon icon={icon}/>}
+            right={() => <IconButton
+                icon="close-circle-outline"
+                onPress={onDisconnectPressed}
+            />}
+        />
     } else {
         switch (status.status) {
             case SessionStatus.Disconnecting:
-                return <View>
-                    <Text>Disconnecting...</Text>
-                </View>
+                return <List.Item
+                    title={t("disconnecting")}
+                />
             case SessionStatus.Failed:
-                return <View>
-                    <Text>Disconnection failed: {status.error}</Text>
-                </View>
+                return <List.Item
+                    title={`${t("an error occurred")}: ${status.error}`}
+                />
 
             default:
                 return null;
