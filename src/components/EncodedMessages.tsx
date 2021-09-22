7d8ac32460b49fc66d891f1689d49acb29c67c11
Manuel Turetta <manuel.turetta94@gmail.com>
2021-09-22 18:39:32 +0100

    Created a component that display the messages that will be included into a transaction

---
+++ src/components/EncodedMessages.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {EncodeObject} from "@cosmjs/proto-signing";
+import {ScrollView, StyleProp, Text, ViewStyle} from "react-native";
+
+export type Props = {
+    /**
+     * The messages to display.
+     */
+    encodeMessages: EncodeObject[]
+    /**
+     * Enables nested scrolling for Android API level 21+.
+     * Nested scrolling is supported by default on iOS.
+     */
+    nestedScrollEnabled?: boolean
+    style?: StyleProp<ViewStyle>
+}
+
+export const EncodedMessages: React.FC<Props> = (props) => {
+    return <ScrollView style={props.style} nestedScrollEnabled={props.nestedScrollEnabled}>
+        <Text>
+            {JSON.stringify(props.encodeMessages, null, 2)}
+        </Text>
+    </ScrollView>
+}
