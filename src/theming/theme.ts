475e57091450cdaf4c02c56c7ec6380dcd597f07
Manuel Turetta <manuel.turetta94@gmail.com>
2021-09-23 17:29:35 +0100

    Extended the Theme object to hold our app specific colors

--- src/theming/theme.ts
+++ src/theming/theme.ts
@@ -3,7 +3,28 @@ import Colors from "../constants/colors";
 
 declare global {
     namespace ReactNativePaper {
+
+        /**
+         * Interface that represents the color used to
+         * provide to the user a feedback about the complexity
+         * of a secret.
+         */
+        interface ComplexityHintColors {
+            weak: string,
+            normal: string,
+            strong: string,
+            veryStrong: string
+        }
+
         interface ThemeColors {
+            /**
+             * Color used for the icons.
+             */
+            icon: string,
+            /**
+             * Color used for the lines like borders.
+             */
+            line: string,
             /**
              * Color used for the buttons with mode = contained
              */
@@ -13,6 +34,11 @@ declare global {
              * app bar.
              */
             appBarContent: string;
+            /**
+             * Colors used to provide to the user a feedback about the
+             * complexity of a password.
+             */
+            passwordComplexity: ComplexityHintColors
         }
 
         interface Spacing {
@@ -46,8 +72,17 @@ export const AppTheme: ReactNativePaper.Theme = {
         ...DefaultTheme.colors,
         background: '#ffffff',
         primary: Colors.DesmosOrange,
-        accent: '#edd459',
+        accent: Colors.DesmosBlue,
+        surface: '#f5f5f5',
+        line: '#e8e8e8',
+        icon: '#afafaf',
         buttonText: '#ffffff',
-        appBarContent: '#ffffff'
+        appBarContent: '#ffffff',
+        passwordComplexity: {
+            weak: "#ecb140",
+            normal: "#379afe",
+            strong: "#1ec490",
+            veryStrong: "#1ec490",
+        }
     },
 };
