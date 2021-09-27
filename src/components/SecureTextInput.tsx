107ff1018eff81fae3e31f11fbc4aedd94200b92
Manuel Turetta <manuel.turetta94@gmail.com>
2021-09-27 15:40:30 +0100

    Restyled the SecureTextInput component

--- src/components/SecureTextInput.tsx
+++ src/components/SecureTextInput.tsx
@@ -1,52 +1,18 @@
 import React, {useState} from "react";
-import {TextInput} from "react-native-paper";
-import {StyleProp, TextStyle} from "react-native";
+import {TextInput, Props as TextInputProps} from "./TextInput";
+import {IconButton, useTheme} from "react-native-paper";
 
-export type Props = {
-    /**
-     * Mode of the TextInput.
-     * - `flat` - flat input with an underline.
-     * - `outlined` - input with an outline.
-     */
-    mode?: 'flat' | 'outlined'
-    /**
-     * The text to use for the floating label.
-     */
-    label?: string
-    /**
-     * The value that the inout should have
-     */
-    value?: string
-    /**
-     * Function called when the text changes.
-     * @param text - The new input text.
-     */
-    onChangeText?: (text: string) => void
-    /**
-     * Whether to style the input with error style.
-     */
-    error?: boolean
-    /**
-     * If true, user won't be able to interact with the component.
-     */
-    disabled?: boolean
-    style?: StyleProp<TextStyle>
-}
 
-export const SecureTextInput: React.FC<Props> = (props) => {
+export const SecureTextInput: React.FC<TextInputProps> = (props) => {
+    const theme = useTheme();
     const [hideText, setHideText] = useState(true)
 
     return <TextInput
-        mode={props.mode}
-        style={props.style}
-        label={props.label}
-        value={props.value}
-        onChangeText={props.onChangeText}
+        {...props}
         secureTextEntry={hideText}
-        error={props.error}
-        disabled={props.disabled}
-        right={<TextInput.Icon
-            name={hideText ? "eye-outline" : "eye-off-outline"}
+        rightElement={<IconButton
+            icon={hideText ? "eye-off-outline" : "eye-outline"}
+            color={theme.colors.icon}
             onPress={() => {
                 setHideText((old) => !old);
             }}/>}
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,23 +1,47 @@
 import React from "react";
-import {StyleProp, StyleSheet, TextStyle, TextInput as NativeTextInput} from "react-native";
+import {StyleProp, StyleSheet, TextStyle, TextInput as NativeTextInput, View, ViewStyle} from "react-native";
 import {makeStyle} from "../theming";
 
-export type Props = React.ComponentProps<typeof NativeTextInput>;
+export type Props = Omit<React.ComponentProps<typeof NativeTextInput>, "style"> & {
+    rightElement?: React.ReactNode | null,
+
+    style?: StyleProp<ViewStyle>,
+    inputStyle?: StyleProp<TextStyle>,
+};
 
 export const TextInput: React.FC<Props> = (props) => {
     const styles = useStyles();
 
-    return <NativeTextInput
-        {...props}
-        style={StyleSheet.compose(styles.input as StyleProp<TextStyle>, props.style)}
-    />
+    return <View style={StyleSheet.compose(styles.container as StyleProp<ViewStyle>, props.style)}>
+        <NativeTextInput
+            {...props}
+            style={StyleSheet.compose(styles.input as StyleProp<TextStyle>, props.inputStyle)}
+        />
+        <View style={styles.right}>
+            {props.rightElement}
+        </View>
+    </View>
 }
 
 const useStyles = makeStyle(theme => ({
-    input: {
+    container: {
+        display: "flex",
+        flexDirection: "row",
         backgroundColor: theme.colors.surface,
         borderRadius: 6,
+        minHeight: 44,
+    },
+    input: {
         fontFamily: "SF-Pro-Text",
-        padding: theme.spacing.s,
+        paddingTop: 0,
+        paddingBottom: 0,
+        paddingLeft: 11,
+        paddingRight: 11,
+        flexGrow: 1,
+    },
+    right: {
+        padding: 0,
+        margin: 0,
+        paddingRight: 11,
     }
 }));
