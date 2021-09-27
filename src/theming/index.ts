7345af688a28c96e3da526517f45713898af253f
Manuel Turetta <manuel.turetta94@gmail.com>
2021-09-27 15:37:53 +0100

    Created an utility function to generate styles using the component props

--- src/theming/index.ts
+++ src/theming/index.ts
@@ -1,6 +1,7 @@
 import {useTheme} from "react-native-paper";
 import {StyleSheet} from "react-native";
 import NamedStyles = StyleSheet.NamedStyles;
+import {useMemo} from "react";
 export * from "./theme"
 
 export function makeStyle<
@@ -10,4 +11,16 @@ export function makeStyle<
         const theme = useTheme();
         return StyleSheet.create(styleProvider(theme));
     };
+}
+
+export function makeStyleWithProps<
+    P,
+    T extends NamedStyles<T> | NamedStyles<any>,
+>(styleProvider: (props: P, theme: ReactNativePaper.Theme) => T): (props: P) => T {
+    return (props: P) => {
+        const theme = useTheme();
+        return useMemo(() => {
+            return StyleSheet.create(styleProvider(props, theme));
+        }, [props, theme]);
+    }
 }
