8d8733db55a1fc2d00354680c014703de882d9ea
Manuel Turetta <manuel.turetta94@gmail.com>
2021-09-27 11:32:51 +0100

    Made the Paragraph font size customizable

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import {StyleProp, StyleSheet, Text, TextStyle} from "react-native";
 import {makeStyle} from "../theming";
 
-export type Props = React.ComponentProps<typeof Text>;
+export type Props = React.ComponentProps<typeof Text> & {
+    fontSize?: 14 | 16
+};
 
 export const Paragraph: React.FC<Props> = (props) => {
     const styles = useStyles();
 
+    let style: StyleProp<TextStyle> = styles.paragraph;
+    if (props.fontSize) {
+        style = StyleSheet.compose(style, {fontSize: props.fontSize})
+    }
+
     return <Text {...props}
-        style={StyleSheet.compose(styles.paragraph as StyleProp<TextStyle>, props.style)}
+        style={StyleSheet.compose(style, props.style)}
     />
 }
 
-const useStyles = makeStyle(_ => ({
+const useStyles = makeStyle(theme => ({
     paragraph: {
         fontFamily: "SF-Pro-Text",
         fontSize: 14,
@@ -21,5 +28,6 @@ const useStyles = makeStyle(_ => ({
         lineHeight: 20,
         letterSpacing: 0.005,
         textAlign: "left",
+        color: theme.colors.text,
     }
 }));
