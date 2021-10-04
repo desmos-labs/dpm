f1bda21736607e8aed9a70e902b84e7b19fc0811
Manuel Turetta <manuel.turetta94@gmail.com>
2021-10-04 10:04:49 +0100

    Suppressed navigator non serializable values warning

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
@@ -11,4 +11,7 @@ import './shim';
 import 'react-native-gesture-handler';
 
 SInfo.setInvalidatedByBiometricEnrollment(true);
+LogBox.ignoreLogs([
+    'Non-serializable values were found in the navigation state',
+]);
 AppRegistry.registerComponent(appName, () => App);
