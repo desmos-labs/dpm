b633ea402b89967fb081e06e4ffec06796bcf9c7
Manuel Turetta <manuel.turetta94@gmail.com>
2021-10-19 19:31:52 +0100

    Added a badge into the home screen to signal if the application is on testnet

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,9 +9,10 @@ import useSelectedAccount from "../hooks/useSelectedAccount";
 import {useTranslation} from "react-i18next";
 import useFetchProfile from "../hooks/useFetchProfile";
 import {makeStyle} from "../theming";
-import {Image} from "react-native";
+import {Image, Text, View} from "react-native";
 import {Snackbar} from "react-native-paper";
 import Clipboard from "@react-native-community/clipboard";
+import {useCurrentChainInfo} from "@desmoslabs/sdk-react";
 
 export type Props = CompositeScreenProps<BottomTabScreenProps<HomeScreensBottomTabsParams, "Home">,
     CompositeScreenProps<DrawerScreenProps<HomeScreensDrawerParams>, StackScreenProps<AccountScreensStackParams>>>;
@@ -23,6 +24,7 @@ export const Home: React.FC<Props> = (props) => {
     const styles = useStyles();
     const profile = useFetchProfile(account.address);
     const [snackBarMessage, setShowSnackbar] = useState<string | null>(null);
+    const currentChain = useCurrentChainInfo();
 
     const openProfileDetails = useCallback(() => {
         navigation.navigate({
@@ -55,6 +57,11 @@ export const Home: React.FC<Props> = (props) => {
     }, [props.navigation])
 
     return <StyledSafeAreaView padding={0}>
+        {currentChain.chainId !== "desmos-mainnet" && (
+            <View style={styles.testnetBadge}>
+                <Text style={styles.testnetText}>TESTNET</Text>
+            </View>)}
+
         <Image
             source={require("../assets/home-background-light.png")}
             resizeMode={"stretch"}
@@ -87,6 +94,20 @@ export const Home: React.FC<Props> = (props) => {
 }
 
 const useStyles = makeStyle(theme => ({
+    testnetBadge: {
+        position: "absolute",
+        bottom: 5,
+        right: 0,
+        padding: theme.spacing.s,
+        borderTopLeftRadius: theme.roundness,
+        borderBottomLeftRadius: theme.roundness,
+        backgroundColor: theme.colors.background,
+        zIndex: 999,
+    },
+    testnetText: {
+        color: theme.colors.primary,
+        fontWeight: "bold",
+    },
     background: {
         position: "absolute",
         width: "100%",
