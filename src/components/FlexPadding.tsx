169fec5fbc4e63fa5756b588cf7f06fc75bc886e
Manuel Turetta <manuel.turetta94@gmail.com>
2021-10-27 12:38:27 +0100

    Removed onPress from FlexPadding

--- src/components/FlexPadding.tsx
+++ src/components/FlexPadding.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import {StyleSheet, TouchableWithoutFeedback, View} from "react-native";
+import {StyleSheet, View} from "react-native";
 
 export type Props = {
     /**
      * Flex value used to generate the padding area.
      */
     flex: number,
-
-    onPress?: () => void,
 }
 
-export const FlexPadding: React.FC<Props> = ({flex, onPress}) => {
+export const FlexPadding: React.FC<Props> = ({flex}) => {
     const style = StyleSheet.create({
         padding: {
             flex,
         }
     })
-    return <TouchableWithoutFeedback
-        style={style.padding}
-        onPress={onPress}>
-        <View style={style.padding}/>
-    </TouchableWithoutFeedback>
+    return <View style={style.padding}/>
 }
