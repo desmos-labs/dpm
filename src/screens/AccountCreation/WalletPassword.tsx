14410d45343629ecba68ada63d5f0d9f08172676
Manuel Turetta <manuel.turetta94@gmail.com>
2021-10-27 12:32:45 +0100

    Password input now take the focus on the create and check password screens

--- src/screens/AccountCreation/WalletPassword.tsx
+++ src/screens/AccountCreation/WalletPassword.tsx
@@ -118,6 +118,7 @@ export default function WalletPassword(props: Props): JSX.Element {
             value={password}
             onChangeText={onPasswordChange}
             onSubmitEditing={onContinuePressed}
+            autoFocus={true}
         />
         {isCreatePassword && <Paragraph style={styles.passwordComplexityHint}>
             {t("password complexity hint")}.
