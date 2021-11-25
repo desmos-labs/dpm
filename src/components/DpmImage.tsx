6dc05fe65d8575e974e9547787d503533339964d
Manuel <manuel.turetta94@gmail.com>
2021-11-25 11:54:35 +0100

    Created a component that shows our images taking in account the current app theme

---
+++ src/components/DpmImage.tsx
@@ -0,0 +1,54 @@
+import React, {useMemo} from "react";
+import {Image} from "react-native";
+import useIsCurrentThemeDark from "../hooks/useIsCurrentThemeDark";
+
+
+type ImageProps = React.ComponentProps<typeof Image>;
+
+export type DpmImages = "no-transaction" | "no-profile" | "success" | "fail" |
+    "no-connection" | "connect-chain" | "connect-mnemonic"
+
+export type Props = Omit<ImageProps, "source"> & {
+    source: DpmImages | ImageProps["source"],
+}
+
+export const DpmImage: React.FC<Props> = (props) => {
+    const darkTheme = useIsCurrentThemeDark();
+
+    const imageSource = useMemo(() => {
+        const typeOfSource = typeof props.source;
+
+        if (typeOfSource === "string") {
+            switch (props.source as DpmImages) {
+                case "no-transaction":
+                    return darkTheme ? require("../assets/no-transaction-dark.png") :
+                        require("../assets/no-profile-light.png");
+                case "no-profile":
+                    return darkTheme ? require("../assets/no-profile-dark.png") :
+                        require("../assets/no-profile-light.png");
+                case "success":
+                    return darkTheme ? require("../assets/result-sucess-dark.png") :
+                        require("../assets/result-sucess-light.png");
+                case "fail":
+                    return darkTheme ? require("../assets/result-fail-dark.png") :
+                        require("../assets/result-fail-light.png");
+                case "no-connection":
+                    return darkTheme ? require("../assets/no-connection-dark.png") :
+                        require("../assets/no-connection-light.png");
+                case "connect-chain":
+                    return darkTheme ? require("../assets/connect_chain_dark.png") :
+                        require("../assets/connect_chain_light.png");
+                case "connect-mnemonic":
+                    return darkTheme ? require("../assets/connect_mnemonic_dark.png") :
+                        require("../assets/connect_mnemonic_light.png");
+            }
+        } else {
+            return props.source
+        }
+    }, [props.source, darkTheme]);
+
+    return <Image
+        {...props}
+        source={imageSource}
+    />
+}
---
+++ src/hooks/useIsCurrentThemeDark.ts
@@ -0,0 +1,10 @@
+import {useTheme} from "react-native-paper";
+
+/**
+ * Hooks that tells if the current theme is dark or not.
+ */
+export default function useIsCurrentThemeDark() {
+    const theme = useTheme()
+
+    return theme.dark;
+}
