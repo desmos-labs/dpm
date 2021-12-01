aa806b3be2f003604dfe113e7fb013d35ba17dbf
Manuel <manuel.turetta94@gmail.com>
2021-12-01 10:42:37 +0100

    Added parsing of MsgMultiSend from graphql

--- src/types/msgtypes.ts
+++ src/types/msgtypes.ts
@@ -1,5 +1,6 @@
 export enum MsgTypes {
     MsgSend = "/cosmos.bank.v1beta1.MsgSend",
+    MsgMultiSend = "/cosmos.bank.v1beta1.MsgMultiSend",
     MsgWithdrawDelegatorReward = "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
     MsgVote = "/cosmos.gov.v1beta1.MsgVote",
     MsgDelegate = "/cosmos.staking.v1beta1.MsgDelegate",
