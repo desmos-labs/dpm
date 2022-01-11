d40266cc30704fed8581dd12984ba21058521605
Manuel <manuel.turetta94@gmail.com>
2022-01-11 19:29:55 +0100

    Updated the Ledger chains applications

--- src/types/ledger.ts
+++ src/types/ledger.ts
@@ -22,6 +22,10 @@ export type LedgerApp = {
      * Name to be displayed to the user.
      */
     uiName: string,
+    /**
+     * Min ledger application version.
+     */
+    minVersion: string,
 }
 
 
@@ -30,25 +34,30 @@ export const LedgerApps: LedgerApp[] = [
         name: "band",
         icon: require("../assets/chains/band.png"),
         uiName: "Band",
+        minVersion: "1.5.3",
     },
     {
-        name: "cosmos",
+        name: "Cosmos",
         icon: require("../assets/chains/cosmos.png"),
         uiName: "Cosmos",
+        minVersion: "1.5.3",
     },
     {
-        name: "crypto.org",
+        name: "Crypto.org Chain",
         icon: require("../assets/chains/crypto_com.png"),
         uiName: "Crypto.org",
+        minVersion: "2.16.5",
     },
     {
         name: "kava",
         icon: require("../assets/chains/kava.png"),
         uiName: "Kava",
+        minVersion: "1.5.3",
     },
     {
-        name: "terra",
+        name: "Terra",
         icon: require("../assets/chains/terra.png"),
         uiName: "Terra",
+        minVersion: "1.0.0",
     },
 ]
