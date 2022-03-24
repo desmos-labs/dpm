3b1e2d5184ebe6db22399e5566422afd408ba81c
ale-mazz <alessandro@forbole.com>
2022-03-24 13:50:15 +0100

    fixed eslint and package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,26 @@
 module.exports = {
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'react-native'],
   extends: ['airbnb', 'airbnb-typescript', 'prettier'],
   env: {
-    amd: true,
-    browser: true,
     node: true,
+    'react-native/react-native': true,
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.eslint.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   rules: {
     'prettier/prettier': 0,
+    'no-console': 1,
     'no-use-before-define': 0,
+    'no-underscore-dangle': 0,
     'global-require': 0,
     '@typescript-eslint/no-use-before-define': 0,
     '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-unused-vars': 0,
     'react/function-component-definition': 0,
     'react/no-unstable-nested-components': 1,
     'react/require-default-props': 0,
