426ebb2e9765895a86c21a2f542c61d01686ebf9
Alessandro Mazzon <alexmazzon@gmail.com>
2022-05-05 14:36:16 +0200

    wip

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-/**
- * @format
- */
-
 import 'text-encoding';
 import './shim';
 import { AppRegistry, LogBox } from 'react-native';
+// import SInfo from 'react-native-sensitive-info';
 import App from './src/App';
 import { name as appName } from './app.json';
 import 'react-native-gesture-handler';
-import SInfo from 'react-native-sensitive-info';
-import 'react-native-gesture-handler';
 
-SInfo.hasEnrolledFingerprints().then((enrolled) => {
+/* SInfo.hasEnrolledFingerprints().then((enrolled) => {
   if (enrolled) {
     SInfo.setInvalidatedByBiometricEnrollment(true);
   }
-});
+}); */
+
 LogBox.ignoreLogs(['Non-serializable values were found in the navigation state']);
 AppRegistry.registerComponent(appName, () => App);
