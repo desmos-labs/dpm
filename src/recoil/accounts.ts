5603d018bcb9830208ba287a8f210d413160a35c
Manuel Turetta <manuel.turetta94@gmail.com>
2023-01-04 12:38:21 -0500

    feat: update account type

--- src/recoil/accounts.ts
+++ src/recoil/accounts.ts
@@ -1,21 +1,41 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 import { getMMKV, MMKVKEYS, setMMKV } from 'lib/MMKVStorage';
 import { Account } from 'types/account';
+import { HdPath } from '@cosmjs/crypto';
+import { WalletType } from 'types/wallet';
 
 /**
  * An atom that holds all the accounts stored in the application.
  */
-const accounts = atom<Record<string, Account>>({
+export const accountsAppState = atom<Record<string, Account>>({
   key: 'accounts',
   default: getMMKV(MMKVKEYS.ACCOUNTS) || {},
   effects: [
-    ({onSet}) => {
-      onSet(newAccounts => {
+    ({ onSet }) => {
+      onSet((newAccounts) => {
         setMMKV(MMKVKEYS.ACCOUNTS, newAccounts);
       });
     },
   ],
 });
 
-
-export default accounts;
+/**
+ * An atom that holds all the accounts addresses.
+ */
+export const accountsHdPathsAppState = selector<HdPath[]>({
+  key: 'accountsAddresses',
+  get: ({ get }) => {
+    const accounts = get(accountsAppState);
+    return Object.values(accounts)
+      .map((account) => {
+        if (
+          account.walletType === WalletType.Ledger ||
+          account.walletType === WalletType.Mnemonic
+        ) {
+          return account.hdPath;
+        }
+        return null;
+      })
+      .filter((value) => value !== null) as HdPath[];
+  },
+});
--- src/types/account.ts
+++ src/types/account.ts
@@ -1,24 +1,64 @@
 import { Algo } from '@cosmjs/amino';
 import { WalletType } from 'types/wallet';
+import { HdPath } from '@cosmjs/crypto';
 
 /**
- * Interface representing a user account.
+ * Interface representing a base user account.
  */
-export interface Account {
+export interface BaseAccount {
   /**
    * Type of the wallet associated with this account.
    */
-  readonly walletType: WalletType,
+  readonly walletType: WalletType;
   /**
    * Account bech32 address.
    */
-  readonly address: string,
+  readonly address: string;
   /**
    * Account public key.
    */
-  readonly pubKey: Uint8Array,
+  readonly pubKey: Uint8Array;
   /**
    * Account public key type.
    */
-  readonly algo: Algo
+  readonly algo: Algo;
 }
+
+/**
+ * Interface representing an account created with a mnemonic.
+ */
+export interface MnemonicAccount extends BaseAccount {
+  readonly walletType: WalletType.Mnemonic;
+  /**
+   * HdPath used to derive the user's private key.
+   */
+  readonly hdPath: HdPath;
+}
+
+/**
+ * Interface representing an account imported through a Ledger device.
+ */
+export interface LedgerAccount extends BaseAccount {
+  readonly walletType: WalletType.Ledger;
+  /**
+   * HdPath used to derive the user's private key.
+   */
+  readonly hdPath: HdPath;
+  /**
+   * Name of the ledger app used to import this account.
+   */
+  readonly ledgerAppName: string;
+}
+
+/**
+ * Interface representing an account imported through Web3Auth.
+ */
+export interface Web3AuthAccount extends BaseAccount {
+  readonly walletType: WalletType.Web3Auth;
+  /**
+   * Login provider used to obtain the user's private key.
+   */
+  readonly loginProvider: string;
+}
+
+export type Account = MnemonicAccount | LedgerAccount | Web3AuthAccount;
