8aba8b85a618a0e6733839204e72382452592df0
Manuel Turetta <manuel.turetta94@gmail.com>
2023-01-06 15:59:20 -0500

    refactor: use getAccounts

--- src/hooks/useSaveAccount.ts
+++ src/hooks/useSaveAccount.ts
@@ -2,14 +2,13 @@ import { useCallback, useMemo } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack/lib/typescript/src/types';
 import { RootNavigatorParamList } from 'navigation/RootNavigator';
-import { useRecoilValue } from 'recoil';
-import { accountsAppState } from '@recoil/accounts';
+import { getAccounts } from '@recoil/accounts';
 import { AccountWithWallet } from 'types/account';
 import ROUTES from 'navigation/routes';
 
 export const useSaveAccount = () => {
   const navigator = useNavigation<StackNavigationProp<RootNavigatorParamList>>();
-  const accounts = useRecoilValue(accountsAppState);
+  const accounts = getAccounts();
   const createWalletPassword = useMemo(() => Object.keys(accounts).length === 0, [accounts]);
 
   const saveAccount = useCallback(
