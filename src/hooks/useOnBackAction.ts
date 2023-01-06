0255c9e95475de467189089b6ee759f41cf58c15
Manuel Turetta <manuel.turetta94@gmail.com>
2023-01-06 10:41:20 -0500

    feat: allow callback without arguments in useOnBackAction

--- src/hooks/useOnBackAction.ts
+++ src/hooks/useOnBackAction.ts
@@ -2,13 +2,14 @@ import { DependencyList, useCallback, useEffect } from 'react';
 import { EventArg, useNavigation } from '@react-navigation/native';
 
 type CallbackArg = EventArg<'beforeRemove', true, { action: { type: string } }>;
+type BackCallback = (event: CallbackArg) => any | (() => any);
 
 /**
  * Hook that execute the provided callback when the user go back from the current screen.
  * @param onBack - Callback called when the user press the back button.
  * @param deps - Deps that are used inside the {@param onBack} callback.
  */
-const useOnBackAction = (onBack: (event?: CallbackArg) => any, deps: DependencyList) => {
+const useOnBackAction = (onBack: BackCallback, deps: DependencyList) => {
   const navigation = useNavigation();
   const beforeRemoveListener = useCallback(
     (e: CallbackArg) => {
