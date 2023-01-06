340d3550218109b1cb9fd6b1f2612c15ceb2a328
Manuel Turetta <manuel.turetta94@gmail.com>
2023-01-06 10:08:24 -0500

    feat: add hook that calls a function when the user go back from a screen

---
+++ src/hooks/useOnBackAction.ts
@@ -0,0 +1,30 @@
+import { DependencyList, useCallback, useEffect } from 'react';
+import { EventArg, useNavigation } from '@react-navigation/native';
+
+type CallbackArg = EventArg<'beforeRemove', true, { action: { type: string } }>;
+
+/**
+ * Hook that execute the provided callback when the user go back from the current screen.
+ * @param onBack - Callback called when the user press the back button.
+ * @param deps - Deps that are used inside the {@param onBack} callback.
+ */
+const useOnBackAction = (onBack: (event?: CallbackArg) => any, deps: DependencyList) => {
+  const navigation = useNavigation();
+  const beforeRemoveListener = useCallback(
+    (e: CallbackArg) => {
+      if (e.data.action.type === 'GO_BACK') {
+        onBack(e);
+      }
+    },
+    [onBack, ...deps],
+  );
+
+  useEffect(() => {
+    navigation.addListener('beforeRemove', beforeRemoveListener);
+    return () => {
+      navigation.removeListener('beforeRemove', beforeRemoveListener);
+    };
+  }, [navigation, beforeRemoveListener]);
+};
+
+export default useOnBackAction;
