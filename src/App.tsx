df84fbb0ffe1f3700e1cd441c326f94279e59cab
Manuel Turetta <manuel.turetta94@gmail.com>
2023-01-10 11:06:16 -0500

    fix: add AppStateProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { RecoilRoot } from 'recoil';
+import { AppStateProvider } from 'contexts/AppContext';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from 'navigation/RootNavigator';
 import ThemeProvider from 'contexts/ThemeContext';
 
 const App = () => (
   <RecoilRoot>
-    <ThemeProvider>
-      <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </ThemeProvider>
+    <AppStateProvider>
+      <ThemeProvider>
+        <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ThemeProvider>
+    </AppStateProvider>
   </RecoilRoot>
 );
 
