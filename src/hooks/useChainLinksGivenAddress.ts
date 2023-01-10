797d8f1155fb1676f3046b628cc575714114172f
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-01-10 11:43:08 -0500

    feat: added chain links caching

--- src/hooks/useChainLinksGivenAddress.ts
+++ src/hooks/useChainLinksGivenAddress.ts
@@ -1,21 +1,50 @@
-import { useQuery } from '@apollo/client';
-import { useMemo } from 'react';
-import GetChainLinks from 'services/graphql/queries/GetChainLinks';
+import { useLazyQuery } from '@apollo/client';
+import React, { useMemo } from 'react';
+import GetAccountChainLinks from 'services/graphql/queries/GetAccountChainLinks';
 import { convertGraphQLChainLink } from 'lib/ChainsUtils';
+import { useStoredChainLinks, useStoreUserChainLinks } from '@recoil/chainLinks';
+import { useActiveAccountAddress } from '@recoil/activeAccountState';
+
 /**
  * Hook to retrieve the selected address profile data.
  */
 const useChainLinksGivenAddress = (address: string | undefined) => {
-  const { data, loading, refetch } = useQuery(GetChainLinks, {
-    variables: { address: address || 'undefined' },
+  const activeAccountAddress = useActiveAccountAddress();
+  const userAddress = address || activeAccountAddress;
+
+  const storeUserChainLinks = useStoreUserChainLinks();
+  const storedChainLinks = useStoredChainLinks();
+  const userChainLinks = useMemo(
+    () => (userAddress ? storedChainLinks[userAddress] || [] : []),
+    [storedChainLinks, userAddress],
+  );
+
+  const [, { loading, refetch }] = useLazyQuery(GetAccountChainLinks, {
+    variables: { address: userAddress },
     fetchPolicy: 'no-cache',
   });
 
-  const chainLinks = useMemo(() => (data?.chainLinks || []).map(convertGraphQLChainLink), [data]);
+  const fetchProfileChainLinks = React.useCallback(async () => {
+    if (!userAddress) {
+      return;
+    }
+
+    const { data } = await refetch({ address: userAddress });
+    if (!data) {
+      return;
+    }
+
+    const { chainLinks } = data;
+    if (userAddress === activeAccountAddress) {
+      // Cache the chain links of the active user
+      storeUserChainLinks(userAddress, chainLinks.map(convertGraphQLChainLink));
+    }
+  }, [userAddress, refetch, storeUserChainLinks, activeAccountAddress]);
+
   return {
-    chainLinks,
+    chainLinks: userChainLinks,
     loading,
-    refetch,
+    refetch: fetchProfileChainLinks,
   };
 };
 
--- src/recoil/chainLinks.ts
+++ src/recoil/chainLinks.ts
@@ -1,41 +1,66 @@
-import { selectorFamily, useRecoilRefresher_UNSTABLE, useRecoilValueLoadable } from 'recoil';
-import GetChainLinks from 'services/graphql/queries/GetChainLinks';
-import { ChainLink } from 'types/chains';
-import useGraphQLClient from 'services/graphql/client';
-import { convertGraphQLChainLink } from 'lib/ChainsUtils';
+import React from 'react';
+import { atom, useRecoilState, useRecoilValue } from 'recoil';
+import { getMMKV, MMKVKEYS, setMMKV } from 'lib/MMKVStorage';
+import { ChainLink } from 'types/desmos';
 
 /**
- * Recoil atom for the user's chain links
+ * Recoil atom that holds all the chain links of all the profiles stored niisde the application.
  */
-const chainLinkState = selectorFamily<ChainLink[], string>({
-  key: 'chainLink',
-  get: (address: string) => async () => {
-    const client = useGraphQLClient();
-    const { data } = await client.query({
-      query: GetChainLinks,
-      variables: {
-        address,
-      },
-      fetchPolicy: 'no-cache',
-    });
-
-    const { chainLinks } = data;
-    return chainLinks.map(convertGraphQLChainLink);
-  },
+export const chainLinksAppState = atom<Record<string, ChainLink[]>>({
+  key: 'chainLinks',
+  default: getMMKV(MMKVKEYS.CHAIN_LINKS) || {},
+  effects: [
+    ({ onSet }) => {
+      onSet((newChainLinks) => {
+        setMMKV(MMKVKEYS.CHAIN_LINKS, newChainLinks);
+      });
+    },
+  ],
 });
 
-export const useChainLinks = (address: string) => {
-  const chainLinksSelector = useRecoilValueLoadable<ChainLink[]>(chainLinkState(address));
+/**
+ * Hook that allows to easily get the currently stored chain links.
+ */
+export const useStoredChainLinks = () => useRecoilValue(chainLinksAppState);
 
-  const refetchChainLinks = useRecoilRefresher_UNSTABLE(chainLinkState(address));
-
-  const { state, contents } = chainLinksSelector;
-  return {
-    loading: state === 'loading',
-    chainLinks: state === 'hasValue' ? contents : [],
-    error: state === 'hasError' ? contents : undefined,
-    refetch: refetchChainLinks,
-  };
+/**
+ * Hook that allows storing a new chain link inside the application state.
+ */
+export const useStoreChainLink = () => {
+  const [, setChainLinks] = useRecoilState(chainLinksAppState);
+  return React.useCallback(
+    (chainLink: ChainLink) => {
+      setChainLinks((currentChainLinks) => {
+        const existing = new Set(currentChainLinks[chainLink.userAddress] || []);
+        existing.add(chainLink);
+        const newChainLinks: Record<string, ChainLink[]> = {
+          ...currentChainLinks,
+        };
+        newChainLinks[chainLink.userAddress] = Array.from(existing);
+        return newChainLinks;
+      });
+    },
+    [setChainLinks],
+  );
 };
 
-export default chainLinkState;
+/**
+ * Hook that allows storing a list of chain links associated with the same user inside the application state.
+ */
+export const useStoreUserChainLinks = () => {
+  const [, setChainLinks] = useRecoilState(chainLinksAppState);
+  return React.useCallback(
+    (user: string, chainLinks: ChainLink[]) => {
+      setChainLinks((currentChainLinks) => {
+        const newChainLinks: Record<string, ChainLink[]> = {
+          ...currentChainLinks,
+        };
+        newChainLinks[user] = chainLinks;
+        return newChainLinks;
+      });
+    },
+    [setChainLinks],
+  );
+};
+
+export default useStoreChainLink;
