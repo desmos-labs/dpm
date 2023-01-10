615f12128936c59b37d7a5ae81363fb9fa2e1f41
Manuel Turetta <manuel.turetta94@gmail.com>
2023-01-10 10:22:30 -0500

    refactor: removed some context from the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { RecoilRoot } from 'recoil';
-import { AppStateProvider } from 'contexts/AppContext';
-import { WalletContextProvider } from 'contexts/WalletConnectContext';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from 'navigation/RootNavigator';
 import ThemeProvider from 'contexts/ThemeContext';
 
 const App = () => (
   <RecoilRoot>
-    <AppStateProvider>
-      <WalletContextProvider>
-        <ThemeProvider>
-          <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
-          <NavigationContainer>
-            <RootNavigator />
-          </NavigationContainer>
-        </ThemeProvider>
-      </WalletContextProvider>
-    </AppStateProvider>
+    <ThemeProvider>
+      <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </ThemeProvider>
   </RecoilRoot>
 );
 
