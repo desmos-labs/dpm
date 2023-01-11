8f713380f2b38ccfa1a5fd1c9720e29b6a646741
Manuel Turetta <manuel.turetta94@gmail.com>
2023-01-11 11:31:12 -0500

    docs: update routes docs

--- src/navigation/routes.ts
+++ src/navigation/routes.ts
@@ -84,6 +84,8 @@ enum ROUTES {
   WALLET_CONNECT_SESSIONS = 'WALLET_CONNECT_SESSIONS',
   /**
    * Home screen.
+   * Screen that allow the user to scan a WalletConnect qr code
+   * to initialize a session.
    */
   SCAN_QR_CODE = 'SCAN_QR_CODE',
   /**
@@ -116,7 +118,7 @@ enum ROUTES {
    */
   BROADCAST_TX = 'BROADCAST_TX',
   /**
-   *
+   * Screen that displays a modal to the user.
    */
   MODAL = 'MODAL',
 }
