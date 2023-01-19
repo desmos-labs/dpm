4e662430284cb4d86e51b390bae6c5f6d01cfe25
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-01-19 13:56:08 -0500

    chore: fixed lint error

--- src/components/ProfileHeader/useHooks.ts
+++ src/components/ProfileHeader/useHooks.ts
@@ -1,7 +1,6 @@
 import { useCallback } from 'react';
 import { launchImageLibrary } from 'react-native-image-picker';
 import { ImagePickerResponse } from 'react-native-image-picker/src/types';
-import { useSetAppState } from '@recoil/appState';
 
 export const usePickPicture = () =>
   useCallback((callback: (value: string | undefined) => void) => {
