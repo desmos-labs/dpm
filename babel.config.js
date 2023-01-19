0c1a1210c5fef179eb5df8674b55a4d4038ea34b
Alessandro Mazzon <alessandro@forbole.com>
2023-01-19 16:27:36 +0100

    build: babel config and package update

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'module:react-native-dotenv',
+    ['@babel/plugin-proposal-numeric-separator'],
     [
       'module-resolver',
       {
