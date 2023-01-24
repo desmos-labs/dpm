0ca450ead8778ef4dcecc5c69b847b9f4116f519
Manuel Turetta <manuel.turetta94@gmail.com>
2023-01-24 15:04:17 -0500

    refactor: removed unused styles

--- src/screens/ImportAccountSelectType/useStyles.ts
+++ src/screens/ImportAccountSelectType/useStyles.ts
@@ -4,13 +4,6 @@ const useStyle = makeStyle((theme) => ({
   background: {
     backgroundColor: theme.colors.background2,
   },
-  topMargin: {
-    marginTop: theme.spacing.l,
-  },
-  bottomMargin: {
-    paddingVertical: theme.spacing.xl,
-    marginBottom: theme.spacing.l,
-  },
 }));
 
 export default useStyle;
