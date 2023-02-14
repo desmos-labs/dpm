0d7c9908bbaeb0a24de2344f36db1af56d1eeffb
Manuel Turetta <manuel.turetta94@gmail.com>
2023-02-13 23:20:34 -0500

    feat: use the usePaginatedData hook to fetch the validators list

--- src/screens/Stacking/index.tsx
+++ src/screens/Stacking/index.tsx
@@ -4,17 +4,27 @@ import React, { FC } from 'react';
 import StyledSafeAreaView from 'components/StyledSafeAreaView';
 import TopBar from 'components/TopBar';
 import { StackScreenProps } from '@react-navigation/stack';
-import PaginatedFlatList, {
-  ListRenderItemInfo,
-} from 'screens/SelectAccount/components/PaginatedFlatList';
+import { ListRenderItemInfo } from 'screens/SelectAccount/components/PaginatedFlatList';
 import { useFetchValidators } from 'screens/Stacking/useHooks';
 import { Validator } from 'types/validator';
 import ValidatorListItem from 'screens/Stacking/components/ValidatorItem';
+import { FlashList } from '@shopify/flash-list';
+import { usePaginatedData } from 'hooks/usePaginatedData';
+import StyledActivityIndicator from 'components/StyledActivityIndicator';
+import TextInput from 'components/TextInput';
 
 type NavProps = StackScreenProps<RootNavigatorParamList, ROUTES.STACKING>;
 
 const Staking: FC<NavProps> = (props) => {
   const loadValidatorPage = useFetchValidators();
+  const { data, loading, fetchMore, refresh, refreshing, updateFilter } = usePaginatedData(
+    loadValidatorPage,
+    { itemsPerPage: 50, updateFilterDebounceTimeMs: 500 },
+    {
+      text: '',
+      votingPowerOrder: 'desc',
+    },
+  );
 
   const renderValidator = React.useCallback(
     ({ item }: ListRenderItemInfo<Validator>) => <ValidatorListItem validator={item} />,
@@ -23,11 +33,30 @@ const Staking: FC<NavProps> = (props) => {
 
   return (
     <StyledSafeAreaView topBar={<TopBar stackProps={props} />}>
-      <PaginatedFlatList
+      <TextInput
+        onChangeText={(e) => {
+          updateFilter((currentFilter) => ({
+            ...currentFilter,
+            text: e.trim(),
+          }));
+        }}
+      />
+
+      <FlashList
+        data={data}
         renderItem={renderValidator}
-        loadPage={loadValidatorPage}
-        itemsPerPage={50}
+        refreshing={refreshing}
+        onRefresh={refresh}
+        onEndReached={fetchMore}
+        onEndReachedThreshold={0.8}
         estimatedItemSize={100}
+        ListFooterComponent={
+          <StyledActivityIndicator
+            animating={loading && !refreshing}
+            hidesWhenStopped
+            size="small"
+          />
+        }
       />
     </StyledSafeAreaView>
   );
--- src/screens/Stacking/useHooks.ts
+++ src/screens/Stacking/useHooks.ts
@@ -2,27 +2,36 @@ import { useApolloClient } from '@apollo/client';
 import React from 'react';
 import { Validator } from 'types/validator';
 import GetValidators from 'services/graphql/queries/GetValidators';
+import { PaginatedResult } from 'hooks/usePaginatedData';
+
+interface FetchValidatorFilter {
+  text: string;
+  votingPowerOrder?: 'asc' | 'desc';
+  monikerOrder?: 'asc' | 'desc';
+}
 
 export const useFetchValidators = () => {
   const client = useApolloClient();
 
   return React.useCallback(
-    async (startIndex: number, endIndex: number): Promise<Validator[] | null> => {
-      console.log('query', startIndex, endIndex);
+    async (
+      offset: number,
+      limit: number,
+      filter?: FetchValidatorFilter,
+    ): Promise<PaginatedResult<Validator>> => {
       const { data } = await client.query({
         query: GetValidators,
         variables: {
-          offset: startIndex,
-          limit: endIndex - startIndex,
+          offset,
+          limit,
+          moniker_ilike: `%${filter?.text?.trim()}%`,
+          moniker_order: filter?.monikerOrder,
+          voting_power_order: filter?.votingPowerOrder,
         },
         fetchPolicy: 'network-only',
       });
 
-      if (data.validator.length === 0) {
-        return null;
-      }
-
-      return data.validator.map(
+      const validators = data.validator.map(
         (validator: any) =>
           ({
             address: validator.validator_descriptions[0].validator_address,
@@ -33,6 +42,10 @@ export const useFetchValidators = () => {
             avatarUrl: validator.validator_descriptions[0].avatar_url ?? undefined,
           } as Validator),
       );
+
+      return {
+        data: validators,
+      };
     },
     [client],
   );
