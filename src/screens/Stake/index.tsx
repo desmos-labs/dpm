f6947188a490a11f084a40deca18a81ebb11bb56
Manuel Turetta <manuel.turetta94@gmail.com>
2023-05-12 16:37:11 -0500

    feat: implemented staking functionality

--- src/screens/Stake/index.tsx
+++ src/screens/Stake/index.tsx
@@ -6,6 +6,21 @@ import React from 'react';
 import StyledSafeAreaView from 'components/StyledSafeAreaView';
 import TopBar from 'components/TopBar';
 import { useTranslation } from 'react-i18next';
+import { View } from 'react-native';
+import Typography from 'components/Typography';
+import AvatarImage from 'components/AvatarImage';
+import { getValidatorAvatar, getValidatorName } from 'lib/ValidatorUtils';
+import Spacer from 'components/Spacer';
+import CoinAmountInput from 'components/CoinAmountInput';
+import { Coin } from '@desmoslabs/desmjs-types/cosmos/base/v1beta1/coin';
+import Flexible from 'components/Flexible';
+import Button from 'components/Button';
+import useBroadcastTx from 'hooks/useBroadcastTx';
+import { MsgDelegateEncodeObject } from '@cosmjs/stargate';
+import { MsgDelegateTypeUrl } from '@desmoslabs/desmjs';
+import { useActiveAccountAddress } from '@recoil/activeAccount';
+import TxMemoInput from 'components/TxMemoInput';
+import useStyles from './useStyles';
 
 export type StakingParams = {
   validator: Validator;
@@ -14,11 +29,103 @@ export type StakingParams = {
 type NavProps = StackScreenProps<RootNavigatorParamList, ROUTES.STAKE>;
 
 const Stake: React.FC<NavProps> = (props) => {
+  const { navigation } = props;
   const { t } = useTranslation('stake');
+  const { validator } = props.route.params;
+  const styles = useStyles();
+  const broadcastTx = useBroadcastTx();
+  const currentAccountAddress = useActiveAccountAddress()!;
+
+  // -------- SCREEN STATE --------
+  const [stakeAmount, setStakeAmount] = React.useState<Coin | undefined>(undefined);
+  const [memo, setMemo] = React.useState('');
+
+  // -------- CALLBACKS --------
+  const onStakeAmountChange = React.useCallback((amount: Coin | undefined, isValid: boolean) => {
+    if (isValid) {
+      setStakeAmount(amount);
+    } else {
+      setStakeAmount(undefined);
+    }
+  }, []);
+
+  const onNextPressed = React.useCallback(() => {
+    if (stakeAmount !== undefined) {
+      broadcastTx(
+        [
+          {
+            typeUrl: MsgDelegateTypeUrl,
+            value: {
+              amount: stakeAmount,
+              validatorAddress: validator.operatorAddress,
+              delegatorAddress: currentAccountAddress,
+            },
+          } as MsgDelegateEncodeObject,
+        ],
+        {
+          memo,
+          onSuccess: () =>
+            navigation.reset({
+              index: 0,
+              routes: [{ name: ROUTES.HOME_TABS }],
+            }),
+        },
+      );
+    }
+  }, [
+    broadcastTx,
+    currentAccountAddress,
+    stakeAmount,
+    validator.operatorAddress,
+    memo,
+    navigation,
+  ]);
+
   return (
-    <StyledSafeAreaView
-      topBar={<TopBar stackProps={props} title={t('stake')} />}
-    ></StyledSafeAreaView>
+    <StyledSafeAreaView topBar={<TopBar stackProps={props} title={t('stake')} />} padding={0}>
+      {/* Staking lock period warning message */}
+      <View style={styles.stakingMessageContainer}>
+        <Typography.Caption style={styles.stakingMessage}>
+          {t('staking will lock your tokens', { count: 14 })}
+        </Typography.Caption>
+      </View>
+
+      {/*
+        Add this container view to restore the default StyledSafeAreaView
+        padding because the staking message shouldn't have padding.
+      */}
+      <View style={styles.content}>
+        {/* Information of the validator to which the user id performing the delegation */}
+        <Typography.Body>{t('stake to')}</Typography.Body>
+        <View style={styles.validatorDetailsContainer}>
+          <AvatarImage source={getValidatorAvatar(validator)} size={32} />
+          <Spacer paddingHorizontal={8} />
+          <Typography.Body>{getValidatorName(validator)}</Typography.Body>
+        </View>
+
+        <Spacer paddingVertical={8} />
+
+        {/* Stake amount */}
+        <CoinAmountInput onChange={onStakeAmountChange} />
+
+        <Spacer paddingVertical={16} />
+
+        {/* Tx memo input */}
+        <Typography.Body>{t('tx:memo')}</Typography.Body>
+        <TxMemoInput value={memo} onChange={setMemo} />
+
+        <Flexible.Padding flex={1} />
+
+        <Button
+          mode={'contained'}
+          style={styles.nextButton}
+          onPress={onNextPressed}
+          disabled={stakeAmount === undefined}
+        >
+          {t('common:next')}
+        </Button>
+      </View>
+    </StyledSafeAreaView>
   );
 };
 
