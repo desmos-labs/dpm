ed83f5e4aeddfb1a9c0e7ce44589ccd544d5d797
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-06-13 17:25:26 -0500

    docs: added missing doc

--- src/hooks/analytics/useTrackEvent.ts
+++ src/hooks/analytics/useTrackEvent.ts
@@ -1,6 +1,9 @@
 import React from 'react';
 import { usePostHog } from 'posthog-react-native';
 
+/**
+ * Hook that returns a function that can be used to track an event.
+ */
 const useTrackEvent = () => {
   const postHog = usePostHog();
 
