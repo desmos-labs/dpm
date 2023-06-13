7afce633181d28d3ace10ebf99bed720002ad7f8
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-06-13 16:48:48 -0500

    feat: added the ability to track additional properties within useTrackScreen

--- src/hooks/analytics/useTrackScreen.ts
+++ src/hooks/analytics/useTrackScreen.ts
@@ -4,8 +4,9 @@ import { usePostHog } from 'posthog-react-native';
 /**
  * Hook to track a screen view.
  * @param screenName - Name of the screen to track.
+ * @param properties - Additional properties to track.
  */
-const useTrackScreen = (screenName: string) => {
+const useTrackScreen = (screenName: string, properties?: any) => {
   const postHog = usePostHog();
 
   React.useEffect(() => {
@@ -13,8 +14,8 @@ const useTrackScreen = (screenName: string) => {
       return;
     }
 
-    postHog.screen(screenName);
-  }, [postHog, screenName]);
+    postHog.screen(screenName, properties);
+  }, [postHog, properties, screenName]);
 };
 
 export default useTrackScreen;
