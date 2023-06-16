56468b2d1f7f25ec758e036f9d198bce132367e8
Manuel Turetta <manuel.turetta94@gmail.com>
2023-06-16 14:53:20 -0500

    refactor: use map instead of switch case in useMessageName

--- src/hooks/messages/useMessageName.ts
+++ src/hooks/messages/useMessageName.ts
@@ -2,6 +2,100 @@ import { Message } from 'types/transactions';
 import { useTranslation } from 'react-i18next';
 import React from 'react';
 
+const NameMap: Record<string, string> = {
+  // Authz module
+  MsgGrant: 'messages.authz:grant',
+  MsgExec: 'messages.authz:exec',
+  MsgRevoke: 'messages.authz:revoke grant',
+
+  // Bank module
+  MsgSend: 'messages.bank:send',
+  MsgMultiSend: 'messages.bank:multi send',
+
+  // Distribution module,
+  MsgWithdrawDelegatorReward: 'messages.distribution:withdraw rewards',
+  MsgSetWithdrawAddress: 'messages.distribution:set withdraw address',
+  MsgWithdrawValidatorCommission: 'messages.distribution:withdraw commission',
+  MsgFundCommunityPool: 'messages.distribution:fund community pool',
+
+  // Feegrant module,
+  MsgGrantAllowance: 'messages.feegrant:grant allowance',
+  MsgRevokeAllowance: 'messages.feegrant:revoke allowance',
+
+  // Gov module,
+  MsgSubmitProposal: 'messages.gov:submit proposal',
+  MsgDeposit: 'messages.gov:deposit',
+  MsgVote: 'messages.gov:vote',
+
+  // IBC,
+  MsgTransfer: 'messages.ibc:ibc transfer',
+
+  // Posts module,
+  MsgCreatePost: 'messages.posts:create post',
+  MsgEditPost: 'messages.posts:edit post',
+  MsgDeletePost: 'messages.posts:delete post',
+  MsgAddPostAttachment: 'messages.posts:add post attachment',
+  MsgRemovePostAttachment: 'messages.posts:remove post attachment',
+  MsgAnswerPoll: 'messages.posts:answer poll',
+
+  // Profiles module,
+  MsgSaveProfile: 'messages.profiles:save profile',
+  MsgDeleteProfile: 'messages.profiles:delete profile',
+  MsgRequestDTagTransfer: 'messages.profiles:request dtag transfer',
+  MsgAcceptDTagTransferRequest: 'messages.profiles:accept dtag transfer',
+  MsgRefuseDTagTransferRequest: 'messages.profiles:refuse dtag transfer',
+  MsgCancelDTagTransferRequest: 'messages.profiles:cancel dtag transfer',
+  MsgLinkApplication: 'messages.profiles:link application',
+  MsgUnlinkApplication: 'messages.profiles:unlink application',
+  MsgLinkChainAccount: 'messages.profiles:link chain account',
+  MsgUnlinkChainAccount: 'messages.profiles:unlink chain account',
+
+  // Reactions,
+  MsgAddReaction: 'messages.reactions:add reaction',
+  MsgRemoveReaction: 'messages.reactions:remove reaction',
+  MsgAddRegisteredReaction: 'messages.reactions:add registered reaction',
+  MsgEditRegisteredReaction: 'messages.reactions:edit registered reaction',
+  MsgRemoveRegisteredReaction: 'messages.reactions:remove registered reaction',
+  MsgSetReactionsParams: 'messages.reactions:set reactions params',
+
+  // Relationships module,
+  MsgCreateRelationship: 'messages.relationships:create relationship',
+  MsgDeleteRelationship: 'messages.relationships:delete relationship',
+  MsgBlockUser: 'messages.relationships:block user',
+  MsgUnblockUser: 'messages.relationships:unblock user',
+
+  // Reports module,
+  MsgCreateReport: 'messages.reports:create report',
+  MsgDeleteReport: 'messages.reports:delete report',
+  MsgSupportStandardReason: 'messages.reports:support standard reason',
+  MsgAddReason: 'messages.reports:add reason',
+  MsgRemoveReason: 'messages.reports:remove reason',
+
+  // Staking module,
+  MsgCreateValidator: 'messages.staking:create validator',
+  MsgEditValidator: 'messages.staking:edit validator',
+  MsgDelegate: 'messages.staking:delegate',
+  MsgUndelegate: 'messages.staking:undelegate',
+  MsgBeginRedelegate: 'messages.staking:redelegate',
+
+  // Subspaces module,
+  MsgCreateSubspace: 'messages.subspaces:create subspace',
+  MsgEditSubspace: 'messages.subspaces:edit subspace',
+  MsgDeleteSubspace: 'messages.subspaces:delete subspace',
+  MsgCreateSection: 'messages.subspaces:create section',
+  MsgEditSection: 'messages.subspaces:edit section',
+  MsgMoveSection: 'messages.subspaces:move section',
+  MsgDeleteSection: 'messages.subspaces:delete section',
+  MsgCreateUserGroup: 'messages.subspaces:create user group',
+  MsgEditUserGroup: 'messages.subspaces:edit user group',
+  MsgMoveUserGroup: 'messages.subspaces:move user group',
+  MsgSetUserGroupPermissions: 'messages.subspaces:set user group permissions',
+  MsgDeleteUserGroup: 'messages.subspaces:delete user group',
+  MsgAddUserToUserGroup: 'messages.subspaces:add user to group',
+  MsgRemoveUserFromUserGroup: 'messages.subspaces:remove user from group',
+  MsgSetUserPermissions: 'messages.subspaces:set user permissions"',
+};
+
 /**
  * Hook that provides a human-readable name of the provided {@link Message}.
  */
@@ -10,170 +104,9 @@ const useMessageName = (message: Message) => {
 
   return React.useMemo(() => {
     const messageType = message.typeUrl.split('.').pop()!;
-    switch (messageType) {
-      // Authz module
-      case 'MsgGrant':
-        return t('messages.authz:grant');
-      case 'MsgExec':
-        return t('messages.authz:exec');
-      case 'MsgRevoke':
-        return t('messages.authz:revoke grant');
-
-      // Bank module
-      case 'MsgSend':
-        return t('messages.bank:send');
-      case 'MsgMultiSend':
-        return t('messages.bank:multi send');
-
-      // Distribution module
-      case 'MsgWithdrawDelegatorReward':
-        return t('messages.distribution:withdraw rewards');
-      case 'MsgSetWithdrawAddress':
-        return t('messages.distribution:set withdraw address');
-      case 'MsgWithdrawValidatorCommission':
-        return t('messages.distribution:withdraw commission');
-      case 'MsgFundCommunityPool':
-        return t('messages.distribution:fund community pool');
-
-      // Feegrant module
-      case 'MsgGrantAllowance':
-        return t('messages.feegrant:grant allowance');
-      case 'MsgRevokeAllowance':
-        return t('messages.feegrant:revoke allowance');
-
-      // Gov module
-      case 'MsgSubmitProposal':
-        return t('messages.gov:submit proposal');
-      case 'MsgDeposit':
-        return t('messages.gov:deposit');
-      case 'MsgVote':
-        return t('messages.gov:vote');
-
-      // IBC
-      case 'MsgTransfer':
-        return t('messages.ibc:ibc transfer');
-
-      // Posts module
-      case 'MsgCreatePost':
-        return t('messages.posts:create post');
-      case 'MsgEditPost':
-        return t('messages.posts:edit post');
-      case 'MsgDeletePost':
-        return t('messages.posts:delete post');
-      case 'MsgAddPostAttachment':
-        return t('messages.posts:add post attachment');
-      case 'MsgRemovePostAttachment':
-        return t('messages.posts:remove post attachment');
-      case 'MsgAnswerPoll':
-        return t('messages.posts:answer poll');
-
-      // Profiles module
-      case 'MsgSaveProfile':
-        return t('messages.profiles:save profile');
-      case 'MsgDeleteProfile':
-        return t('messages.profiles:delete profile');
-      case 'MsgRequestDTagTransfer':
-        return t('messages.profiles:request dtag transfer');
-      case 'MsgAcceptDTagTransferRequest':
-        return t('messages.profiles:accept dtag transfer');
-      case 'MsgRefuseDTagTransferRequest':
-        return t('messages.profiles:refuse dtag transfer');
-      case 'MsgCancelDTagTransferRequest':
-        return t('messages.profiles:cancel dtag transfer');
-      case 'MsgLinkApplication':
-        return t('messages.profiles:link application');
-      case 'MsgUnlinkApplication':
-        return t('messages.profiles:unlink application');
-      case 'MsgLinkChainAccount':
-        return t('messages.profiles:link chain account');
-      case 'MsgUnlinkChainAccount':
-        return t('messages.profiles:unlink chain account');
-
-      // Reactions
-      case 'MsgAddReaction':
-        return t('messages.reactions:add reaction');
-      case 'MsgRemoveReaction':
-        return t('messages.reactions:remove reaction');
-      case 'MsgAddRegisteredReaction':
-        return t('messages.reactions:add registered reaction');
-      case 'MsgEditRegisteredReaction':
-        return t('messages.reactions:edit registered reaction');
-      case 'MsgRemoveRegisteredReaction':
-        return t('messages.reactions:remove registered reaction');
-      case 'MsgSetReactionsParams':
-        return t('messages.reactions:set reactions params');
-
-      // Relationships module
-      case 'MsgCreateRelationship':
-        return t('messages.relationships:create relationship');
-      case 'MsgDeleteRelationship':
-        return t('messages.relationships:delete relationship');
-      case 'MsgBlockUser':
-        return t('messages.relationships:block user');
-      case 'MsgUnblockUser':
-        return t('messages.relationships:unblock user');
-
-      // Reports module
-      case 'MsgCreateReport':
-        return t('messages.reports:create report');
-      case 'MsgDeleteReport':
-        return t('messages.reports:delete report');
-      case 'MsgSupportStandardReason':
-        return t('messages.reports:support standard reason');
-      case 'MsgAddReason':
-        return t('messages.reports:add reason');
-      case 'MsgRemoveReason':
-        return t('messages.reports:remove reason');
-
-      // Staking module
-      case 'MsgCreateValidator':
-        return t('messages.staking:create validator');
-      case 'MsgEditValidator':
-        return t('messages.staking:edit validator');
-      case 'MsgDelegate':
-        return t('messages.staking:delegate');
-      case 'MsgUndelegate':
-        return t('messages.staking:undelegate');
-      case 'MsgBeginRedelegate':
-        return t('messages.staking:redelegate');
-
-      // Subspaces module
-      case 'MsgCreateSubspace':
-        return t('messages.subspaces:create subspace');
-      case 'MsgEditSubspace':
-        return t('messages.subspaces:edit subspace');
-      case 'MsgDeleteSubspace':
-        return t('messages.subspaces:delete subspace');
-      case 'MsgCreateSection':
-        return t('messages.subspaces:create section');
-      case 'MsgEditSection':
-        return t('messages.subspaces:edit section');
-      case 'MsgMoveSection':
-        return t('messages.subspaces:move section');
-      case 'MsgDeleteSection':
-        return t('messages.subspaces:delete section');
-      case 'MsgCreateUserGroup':
-        return t('messages.subspaces:create user group');
-      case 'MsgEditUserGroup':
-        return t('messages.subspaces:edit user group');
-      case 'MsgMoveUserGroup':
-        return t('messages.subspaces:move user group');
-      case 'MsgSetUserGroupPermissions':
-        return t('messages.subspaces:set user group permissions');
-      case 'MsgDeleteUserGroup':
-        return t('messages.subspaces:delete user group');
-      case 'MsgAddUserToUserGroup':
-        return t('messages.subspaces:add user to group');
-      case 'MsgRemoveUserFromUserGroup':
-        return t('messages.subspaces:remove user from group');
-      case 'MsgSetUserPermissions':
-        return t('messages.subspaces:set user permissions"');
-
-      // Unsupported message.
-      default:
-        return messageType;
-    }
-  }, [t, message.typeUrl]);
+    const translationKey = NameMap[messageType];
+    return translationKey ? t(translationKey) : messageType;
+  }, [message.typeUrl, t]);
 };
 
 export default useMessageName;
