0355f28a9c5c32268f12679a9efcf22cfd8a5ef2
Manuel Turetta <manuel.turetta94@gmail.com>
2023-06-16 12:08:56 -0500

    feat: add a component to display an address that can be copied on press

---
+++ src/components/CopiableAddress/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Clipboard from '@react-native-community/clipboard';
+import { useShowSnackBar } from 'lib/SnackBarProvider/recoil';
+import Typography from 'components/Typography';
+import useStyles from './useStyles';
+
+export interface Props {
+  /**
+   * The address to display.
+   */
+  readonly address: string;
+}
+
+/**
+ * Component that shows a address that when clicked copy the
+ * value into the clipboard.
+ */
+const CopiableAddress: React.FC<Props> = ({ address }) => {
+  const { t } = useTranslation();
+  const showSnackBar = useShowSnackBar();
+  const styles = useStyles();
+
+  const onPress = React.useCallback(() => {
+    Clipboard.setString(address);
+    showSnackBar(t('value copied'));
+  }, [t, address, showSnackBar]);
+
+  return (
+    <Typography.Regular14
+      style={styles.text}
+      numberOfLines={1}
+      ellipsizeMode={'middle'}
+      onPress={onPress}
+    >
+      {address}
+    </Typography.Regular14>
+  );
+};
+
+export default CopiableAddress;
