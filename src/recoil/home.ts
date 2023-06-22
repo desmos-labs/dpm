da4ebc798957a7b3d2e5c06bd630aa26ae325232
Manuel Turetta <manuel.turetta94@gmail.com>
2023-06-22 15:08:59 -0500

    docs: update doc

--- src/recoil/home.ts
+++ src/recoil/home.ts
@@ -3,9 +3,9 @@ import { atom, useRecoilValue, useSetRecoilState } from 'recoil';
 /**
  * Recoil that holds a boolean flag that tells if the home should
  * be refreshed.
- * NOTE: This is a workaround to force the home refresh until we have
+ * NOTE: This is a workaround to force the home to refresh until we have
  * a proper cache in place that can be updated after an operation that
- * affect the data displayed in the Home screen has been performed
+ * affect the data displayed in the Home screen.
  */
 const homeShouldReloadData = atom<boolean>({
   key: 'homeShouldReloadData',
