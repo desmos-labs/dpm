ade859c12033190bb2cafe98f7b1a33e792e85a1
Manuel Turetta <manuel.turetta94@gmail.com>
2023-07-04 11:21:41 -0500

    refactor: update CreateWallet screen ui

--- src/screens/CreateWalletPassword/useStyles.ts
+++ src/screens/CreateWalletPassword/useStyles.ts
@@ -1,9 +1,6 @@
 import { makeStyle } from 'config/theme';
 
 const useStyles = makeStyle((theme) => ({
-  root: {
-    paddingTop: 0,
-  },
   passwordLabel: {
     display: 'flex',
     flexDirection: 'row',
