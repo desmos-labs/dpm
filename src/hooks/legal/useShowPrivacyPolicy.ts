e814820ea00260b6db7116dc52d360fa6b144038
Manuel Turetta <manuel.turetta94@gmail.com>
2023-07-19 12:24:49 -0500

    feat: show privacy policy and tos from desmos site

--- src/hooks/legal/useShowPrivacyPolicy.ts
+++ src/hooks/legal/useShowPrivacyPolicy.ts
@@ -1,20 +1,9 @@
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack/lib/typescript/src/types';
-import { RootNavigatorParamList } from 'navigation/RootNavigator';
-import ROUTES from 'navigation/routes';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Linking } from 'react-native';
 
-const useShowPrivacyPolicy = () => {
-  const { t } = useTranslation('legal');
-  const navigation = useNavigation<StackNavigationProp<RootNavigatorParamList>>();
-
-  return React.useCallback(() => {
-    navigation.navigate(ROUTES.MARKDOWN_TEXT, {
-      title: t('privacy policy'),
-      fileName: 'privacy.md',
-    });
-  }, [navigation, t]);
-};
+const useShowPrivacyPolicy = () =>
+  React.useCallback(() => {
+    Linking.openURL('https://desmos.network/dpm/privacy');
+  }, []);
 
 export default useShowPrivacyPolicy;
