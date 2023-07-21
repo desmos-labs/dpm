55d69e08f922f04c3dfdb06f0fa00f6eef2e66fb
Manuel Turetta <manuel.turetta94@gmail.com>
2023-07-21 17:14:57 -0500

    feat: allow the button of a SingleButtonModal to be disabled

--- src/modals/SingleButtonModal/index.tsx
+++ src/modals/SingleButtonModal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import Typography from 'components/Typography';
 import DpmImage, { DPMImageProps } from 'components/DPMImage';
@@ -28,25 +28,53 @@ export type SingleButtonModalParams = {
    * Function called when the user clicks on the button.
    */
   action?: () => void;
+  /**
+   * Amount of milliseconds that the button will remain disabled
+   * before becoming enable.
+   */
+  disableButtonFor?: number;
 };
 
 const SingleButtonModal: React.FC<ModalComponentProps<SingleButtonModalParams>> = (props) => {
   const { params, closeModal } = props;
   const styles = useStyles(props.params);
 
-  const btnAction = useCallback(() => {
+  // -------- STATES --------
+
+  const [buttonDisabled, setButtonDisabled] = React.useState(params.disableButtonFor !== undefined);
+
+  // -------- CALLBACKS --------
+
+  const btnAction = React.useCallback(() => {
     closeModal();
     if (params.action) {
       params.action();
     }
   }, [params, closeModal]);
 
+  // -------- EFFECTS --------
+
+  React.useEffect(() => {
+    if (params.disableButtonFor) {
+      setTimeout(() => {
+        setButtonDisabled(false);
+      }, params.disableButtonFor);
+    }
+  }, [params.disableButtonFor]);
+
   return (
     <View>
       {params.image && <DpmImage style={styles.image} resizeMode="contain" source={params.image} />}
       <Typography.Title style={styles.title}>{params.title}</Typography.Title>
       <Typography.Body style={styles.message}>{params.message}</Typography.Body>
-      <Button style={styles.button} mode="contained" onPress={btnAction}>
+      <Button
+        style={styles.button}
+        mode="contained"
+        onPress={btnAction}
+        disabled={buttonDisabled}
+        loading={buttonDisabled}
+        labelStyle={{ overflow: 'visible' }}
+      >
         {params.actionLabel}
       </Button>
     </View>
