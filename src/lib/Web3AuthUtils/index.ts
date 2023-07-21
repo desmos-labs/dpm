33c35445d2323b1cc70b7896651b9d912eeece36
Manuel <manuel.turetta94@gmail.com>
2023-07-21 10:08:55 -0500

    fix: Web3Auth account import (#224)
    ## Description
    
    Closes: DPM-126
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    - [ ] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] provided a link to the relevant issue or specification
    - [ ] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed all author checklist items have been addressed

--- src/lib/Web3AuthUtils/index.ts
+++ src/lib/Web3AuthUtils/index.ts
@@ -1,15 +1,16 @@
-import Web3Auth, { OPENLOGIN_NETWORK, SdkLoginParams } from '@web3auth/react-native-sdk';
+import { OPENLOGIN_NETWORK, SdkLoginParams, Web3Auth } from '@desmoslabs/desmjs-web3auth-mobile';
 import * as WebBrowser from '@toruslabs/react-native-web-browser';
 // eslint-disable-next-line import/no-unresolved
 import { WEB3_AUTH_CLIENT_ID_MAINNET, WEB3_AUTH_CLIENT_ID_TESTNET } from '@env';
 import { Web3AuthLoginProvider } from 'types/web3auth';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 // Remember to change this value also in android/app/src/main/AndroidManifest.xml.
 export const Web3authScheme = 'dpmweb3auth';
 export const Web3authResolveRedirectUrl = `${Web3authScheme}://openlogin`;
 
 export const newWeb3AuthClient = () =>
-  new Web3Auth(WebBrowser, {
+  new Web3Auth(WebBrowser, EncryptedStorage, {
     clientId: __DEV__ ? WEB3_AUTH_CLIENT_ID_TESTNET : WEB3_AUTH_CLIENT_ID_MAINNET,
     network: __DEV__ ? OPENLOGIN_NETWORK.TESTNET : OPENLOGIN_NETWORK.CYAN,
   });
