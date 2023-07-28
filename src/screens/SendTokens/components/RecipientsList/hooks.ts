4e6564872987fe37514bfd3c211dffee00696d1a
Manuel Turetta <manuel.turetta94@gmail.com>
2023-07-28 09:38:44 -0500

    docs: add documentation to useFetchProfiles

--- src/screens/SendTokens/components/RecipientsList/hooks.ts
+++ src/screens/SendTokens/components/RecipientsList/hooks.ts
@@ -8,6 +8,9 @@ import GetProfilesFromNickNameOrDtagOrAddress from 'services/graphql/queries/Get
  * Hook that provides the function that can be used
  */
 const useFetchProfiles = () => {
+  // Here we use useApolloClient instead of useLazyQuery
+  // to force the returned callback to change when the client instance changes
+  // so that the usePaginatedData hook can properly update the data.
   const apolloClient = useApolloClient();
 
   return React.useCallback<FetchDataFunction<DesmosProfile, string>>(
@@ -20,6 +23,7 @@ const useFetchProfiles = () => {
       }
 
       let addressFilter = '';
+      // Perform an address search only if we have an address.
       if (filter.indexOf('desmos1') === 0 && filter.length === 45) {
         addressFilter = filter;
       }
