d4480ac6dd4b2aca211828bfc5e0228f59ddd43a
Alessandro Mazzon <alessandro@forbole.com>
2023-08-22 17:35:59 +0200

    chore: fix eslint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import 'text-encoding';
 import './shim';
 import { AppRegistry, LogBox } from 'react-native';
+import setupBackgroundNotificationsHandler from 'hooks/notifications/setupBackgroundNotificationsHandler';
 import App from './src/App';
 import { name as appName } from './app.json';
 import 'react-native-gesture-handler';
 import './src/assets/locales/i18n';
 import AppSilent from './src/AppSilent';
-import setupBackgroundNotificationsHandler from 'hooks/notifications/setupBackgroundNotificationsHandler';
 
 LogBox.ignoreLogs(['Non-serializable values were found in the navigation state']);
 
