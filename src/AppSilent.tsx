f4dbc187da1e9b9dc1d3d5e7346136e73555875b
Manuel Turetta <manuel.turetta94@gmail.com>
2023-08-22 13:41:13 -0500

    docs: documented AppSilent component

--- src/AppSilent.tsx
+++ src/AppSilent.tsx
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
 import RNBootSplash from 'react-native-bootsplash';
 
+/**
+ * Component that will be rendered exclusively on iOS to
+ * initialize the application in the background and manage notifications.
+ */
 const AppSilent = () => {
   useEffect(() => {
     RNBootSplash.hide();
