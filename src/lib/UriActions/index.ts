535294bb3e981fb1784a51f81c381b082df4e34f
Manuel Turetta <manuel.turetta94@gmail.com>
2023-08-23 13:31:21 -0500

    feat: allow to cache a global UriAction

--- src/lib/UriActions/index.ts
+++ src/lib/UriActions/index.ts
@@ -2,6 +2,33 @@ import { GenericActionUri, UriAction, UriActionType, UserAddressActionUri } from
 
 const DPM_URI_PROTOCOL = 'dpm:';
 
+let CachedUriAction: UriAction | undefined;
+
+/**
+ * Updates the cached uri action.
+ * @param action - The new uri action.
+ */
+export const setCachedUriAction = (action: UriAction): void => {
+  CachedUriAction = action;
+};
+
+/**
+ * Gets the cached uri action.
+ * After getting the cached uri action, it will be
+ * cleared.
+ */
+export const getCachedUriAction = (): UriAction | undefined => {
+  const action = CachedUriAction;
+  CachedUriAction = undefined;
+  return action;
+};
+
+/**
+ * Checks if there is a pending {@link UriAction} that needs
+ * to be handled.
+ */
+export const isUriActionPending = (): boolean => CachedUriAction !== undefined;
+
 /**
  * Validates if the given address is a valid Desmos Bech32 address.
  * @param address - The address to be validated.
