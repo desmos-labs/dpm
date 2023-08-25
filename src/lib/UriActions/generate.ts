15e57a34e2033deed09a271c53fd6089776a4a1d
Manuel Turetta <manuel.turetta94@gmail.com>
2023-08-25 14:21:53 -0500

    feat: added support to generate deep links for other actions

--- src/lib/UriActions/generate.ts
+++ src/lib/UriActions/generate.ts
@@ -9,24 +9,41 @@ const axiosInstance = axios.create({
 });
 
 /**
+ * Function that generates the url to perform the request to generate a deep link.
+ * @param uriAction - The action that will be used to generate the deep link.
+ */
+const generateGenerationUrl = (uriAction: UriAction): string | undefined => {
+  switch (uriAction.type) {
+    case UriActionType.Generic:
+      return `${uriAction.address}?chain_type=${uriAction.chainType}`;
+    case UriActionType.ViewProfile:
+      return `${uriAction.address}/view_profile?chain_type=${uriAction.chainType}`;
+    case UriActionType.SendTokens:
+      return `${uriAction.address}/send?chain_type=${uriAction.chainType}${
+        uriAction.amount ? `&amount=${uriAction.amount.amount}${uriAction.amount.denom}` : ''
+      }`;
+    default:
+      return undefined;
+  }
+};
+
+/**
  * Functions that generates a deep link from an {@link UriAction}.
  * @param uriAction - The action that will be used to generate the deep link.
  */
 export const generateUriActionUrl = async (
   uriAction: UriAction,
 ): Promise<Result<string, Error>> => {
-  switch (uriAction.type) {
-    case UriActionType.Generic:
-      return ResultAsync.fromPromise(
-        axiosInstance
-          .get(`${uriAction.address}?chain_type=${uriAction.chainType}`)
-          .then((r) => r.data.deep_link as string),
-        (e) => (e as Error) ?? Error('Error while generating uri'),
-      );
-    // TODO: Support the rest of the actions after we have the api documentation.
-    default:
-      return err(Error(`Unsupported URI action type: ${uriAction.type}`));
+  const getUrl = generateGenerationUrl(uriAction);
+
+  if (getUrl === undefined) {
+    return err(Error(`Unsupported URI action type: ${uriAction.type}`));
   }
+
+  return ResultAsync.fromPromise(
+    axiosInstance.get(getUrl).then((r) => r.data.deep_link as string),
+    (e) => (e as Error) ?? Error('Error while generating uri'),
+  );
 };
 
 /**
