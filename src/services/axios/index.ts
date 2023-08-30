662723db4b9f85e11cfe9726ac094d50d2dab1ed
Manuel Turetta <manuel.turetta94@gmail.com>
2023-08-30 09:52:27 -0500

    chore: replaced api server ip with a proper hostname

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -4,8 +4,7 @@ import axios from 'axios';
  * Axios instance to interact with the DPM API.
  */
 const dpmApiAxiosInstance = axios.create({
-  // TODO: Use a proper domain name instead of the ip.
-  baseURL: 'http://57.128.144.235:33000',
+  baseURL: 'https://api-dpm.desmos.network',
   timeout: 1500,
 });
 
