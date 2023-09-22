8e2aaf0ec853863e62f34fb86ab6e6e90f33fdba
Manuel <manuel.turetta94@gmail.com>
2023-09-22 12:50:41 -0500

    fix: validator description text color (#243)
    ## Description
    
    Closes: DPM-154
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    This PR corrects the text color of the validator description when the
    application is in dark mode. It also enhances the component responsible
    for rendering markdown by applying additional styles for various
    markdown tags.
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    - [x] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] provided a link to the relevant issue or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed all author checklist items have been addressed
    
    (cherry picked from commit 414c5a236fafd12d025154d582a5d1ee5bf239fd)

--- src/components/StyledMarkdown/useStyles.ts
+++ src/components/StyledMarkdown/useStyles.ts
@@ -1,10 +1,43 @@
 import { makeStyle } from 'config/theme';
 import {
+  TypographyConfigH1,
+  TypographyConfigH2,
+  TypographyConfigH4,
+  TypographyConfigH5,
+  TypographyConfigH6,
   TypographyConfigRegular16,
   TypographyConfigSemiBold16,
 } from 'components/Typography/config';
 
 const useStyles = makeStyle((theme) => ({
+  heading1: {
+    ...TypographyConfigH1,
+    color: theme.colors.text,
+  },
+  heading2: {
+    ...TypographyConfigH2,
+    color: theme.colors.text,
+  },
+  heading3: {
+    ...TypographyConfigH2,
+    color: theme.colors.text,
+  },
+  heading4: {
+    ...TypographyConfigH4,
+    color: theme.colors.text,
+  },
+  heading5: {
+    ...TypographyConfigH5,
+    color: theme.colors.text,
+  },
+  heading6: {
+    ...TypographyConfigH6,
+    color: theme.colors.text,
+  },
+  body: {
+    ...TypographyConfigRegular16,
+    color: theme.colors.text,
+  },
   link: {
     color: theme.colors.accent,
   },
