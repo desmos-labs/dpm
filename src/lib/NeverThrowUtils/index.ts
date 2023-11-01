37e5b0dd9188472050c5c24cd859b528d97ed07b
Riccardo <riccardo.montagnin@gmail.com>
2023-11-01 09:56:34 -0500

    feat: allow to hide social login options with feature flag (#252)
    ## Description
    
    This PR introduces the usage of a new `hideSocialLoginsOnVersion`
    feature flag that allows to hide social login options (Google, Apple,
    Twitter, etc) from the application.
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    - [x] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [x] provided a link to the relevant issue or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed all author checklist items have been addressed

---
+++ src/lib/NeverThrowUtils/index.ts
@@ -0,0 +1,27 @@
+import { Result, ResultAsync } from 'neverthrow';
+
+/**
+ * Utility function to wrap a promise in a Result.
+ * @param promise - The promise to wrap.
+ * @param errorMessage - Error message returned if the promise rejects with an object
+ * that is not an instance of {@link Error}.
+ */
+export const promiseToResult = <T>(
+  promise: Promise<T>,
+  errorMessage: string,
+): ResultAsync<T, Error> =>
+  ResultAsync.fromPromise(promise, (e: any) =>
+    e?.message ? (e as Error) : new Error(errorMessage),
+  );
+
+/**
+ * Function to unwrap the value from {@link Result} object.
+ * This function will throw the error if the {@link Result} object is an error.
+ * @param result - The Result object to unwrap.
+ */
+export const unwrapResult = <T>(result: Result<T, Error>): T => {
+  if (result.isErr()) {
+    throw result.error;
+  }
+  return result.value;
+};
